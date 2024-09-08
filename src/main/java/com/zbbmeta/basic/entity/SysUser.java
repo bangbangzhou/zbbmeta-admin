8b3d23c1c693e9788f763726f7bbf24988c960a5
springboot葵花宝典 <zbbmeta@qq.com>
2024-09-08 21:28:24 +0800

    整合knife4j

--- src/main/java/com/zbbmeta/basic/entity/SysUser.java
+++ src/main/java/com/zbbmeta/basic/entity/SysUser.java
@@ -7,44 +7,104 @@ import com.baomidou.mybatisplus.annotation.TableName;
 import java.io.Serializable;
 import java.util.Date;
 
-import com.zbbmeta.basic.BasePojo;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
+ * 用户信息表
  * @TableName sys_user
  */
+@ApiModel(description = "用户对象 sys_user")
 @TableName(value ="sys_user")
 @Data
-public class SysUser extends BasePojo {
-    @TableId(type=IdType.ASSIGN_ID)
+public class SysUser implements Serializable {
+    /**
+     * 用户ID
+     */
+    @TableId(type = IdType.AUTO)
+    @ApiModelProperty("用户ID")
     private Long userId;
 
+    /**
+     * 部门ID
+     */
+    @ApiModelProperty("部门ID")
     private Long deptId;
 
+    /**
+     * 用户账号
+     */
+    @ApiModelProperty("用户账号")
     private String userName;
 
+    /**
+     * 用户昵称
+     */
+    @ApiModelProperty("用户昵称")
     private String nickName;
 
+    /**
+     * 用户类型（00系统用户）
+     */
+    @ApiModelProperty("用户类型（00系统用户）")
     private String userType;
 
+    /**
+     * 用户邮箱
+     */
+    @ApiModelProperty("用户邮箱")
     private String email;
 
+    /**
+     * 手机号码
+     */
+    @ApiModelProperty("手机号码")
     private String phonenumber;
 
+    /**
+     * 用户性别（0男 1女 2未知）
+     */
+    @ApiModelProperty("用户性别（0男 1女 2未知）")
     private String sex;
 
+    /**
+     * 头像地址
+     */
+    @ApiModelProperty("头像地址")
     private String avatar;
 
+    /**
+     * 密码
+     */
+    @ApiModelProperty("密码")
     private String password;
 
+    /**
+     * 帐号状态（0正常 1停用）
+     */
+    @ApiModelProperty("帐号状态（0正常 1停用）")
     private String status;
 
+    /**
+     * 删除标志（0代表存在 2代表删除）
+     */
+    @ApiModelProperty("删除标志（0代表存在 2代表删除）")
     private String delFlag;
 
+    /**
+     * 最后登录IP
+     */
+    @ApiModelProperty("最后登录IP")
     private String loginIp;
 
+    /**
+     * 最后登录时间
+     */
+    @ApiModelProperty("最后登录时间")
     private Date loginDate;
 
 
+    @TableField(exist = false)
     private static final long serialVersionUID = 1L;
 }
